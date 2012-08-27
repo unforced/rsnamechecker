6332c98a44ced850b4f3d855e702f06ff8555a7b
Aaron Neyer <aaronneyer@gmail.com>
2012-08-27 09:52:33 -0400

    Changes it to run every 10 minutes, and adds a brief explanatory paragraph.

--- app/models/rs_name.rb
+++ app/models/rs_name.rb
@@ -5,7 +5,7 @@ class RsName < ActiveRecord::Base
 	validates :name, format: { with: /\A[\w ]{1,12}\z/ }
 
 	def self.check_all_names
-		names = RsName.all.select{|n| !n.notified}.collect{|n| n.name.gsub(' ', '_')}.join(' ')
+		names = RsName.all.select{|n| !n.notified}.collect{|n| n.name.gsub(' ', '_')}.uniq.join(' ')
 		results = `java -jar script/namechecker.jar #{APP_CONFIG['rs_username']} #{APP_CONFIG['rs_password']} #{names}`.split("\n").collect{|x| x.split("===")}
 		available_names = results.collect{|result| RsName.where(:name => result[0], :notified => false) if result[1]=="OK"}.flatten.compact
 		available_names.each do |rs_name|
--- config/schedule.rb
+++ config/schedule.rb
@@ -1,3 +1,3 @@
-every 5.minutes do
+every 10.minutes do
 	runner "RsName.check_all_names"
 end
